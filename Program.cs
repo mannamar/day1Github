2a5f86120ed57ba2c940e8f8985a6f6b99245e55
Amardeep Mann <amar2slick@gmail.com>
2022-10-17 10:40:40 -0700

    Removed third line

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("One more thing");
+Console.WriteLine("Cool I think this works now");
